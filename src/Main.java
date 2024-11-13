0c14d6333aab394031d7b8615dced96516ede583
YimoSsDev <116626946+YimoSsDev@users.noreply.github.com>
2024-11-13 16:59:37 +0100

    Vigenere fonctionnel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Test de chiffrement de Vigenère
+        String message = "HELLO WORLD";
+        String key = "KEY";
+
+        String encryptedMessage = VigenereCipher.chiffrer(message, key);
+        System.out.println("Message chiffré : " + encryptedMessage);
+
+        String decryptedMessage = VigenereCipher.dechiffrer(encryptedMessage, key);
+        System.out.println("Message déchiffré : " + decryptedMessage);
     }
 }
